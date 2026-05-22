3bb2fe839ddac156401a1dbad12984e7a45fd26f
hendisf <hendispurnomo@gmail.com>
2026-05-22 12:15:43 +0700

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -3,7 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+if (!supabaseUrl || !supabaseKey) {
+  console.warn('Supabase env missing')
+}
+
+export const supabase =
+  supabaseUrl && supabaseKey
+    ? createClient(supabaseUrl, supabaseKey)
+    : null
